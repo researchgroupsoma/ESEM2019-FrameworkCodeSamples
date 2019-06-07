2fc6d83dabf1cb37d0b3a350ddfc6fd5bdae6852
Gabriel Menezes <gabrielufmscc@gmail.com>
2019-06-07 07:56:17 -0400

    Delecoes

--- Versoes/buscaVersaoDoAndroid/baixarRepositorios.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-cat 'googlesamples.txt' | while read LINE; do
-    echo $LINE
-    git clone $LINE
-done
--- Versoes/buscaVersaoDoSpring/baixarRepositorios.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-cat 'springsamples.txt' | while read LINE; do
-    echo $LINE
-    cd repositorios
-    git clone $LINE
-    cd ..
-done
